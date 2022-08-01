f4e7857a71db5a6fb1414ed9cf199715875d5492
burak <design.burakk@gmail.com>
2022-08-01 10:14:12 +0300

    first commit by burak baba

---
+++ app/src/main/java/com/example/motion/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.motion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.res.ResourcesCompat
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+
+        button.setOnClickListener {
+            MotionToast.createToast(this,
+                "Hurray success 😍",
+                "Upload Completed successfully!",
+                MotionToastStyle.SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+        }
+    }
+}
