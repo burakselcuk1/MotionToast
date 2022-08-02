7f5e731fc4bf10027b4389537cdb4ab7a86bb370
burak <design.burakk@gmail.com>
2022-08-02 12:52:31 +0300

    first commit by burak baba

--- app/src/main/java/com/example/motion/MainActivity.kt
+++ app/src/main/java/com/example/motion/MainActivity.kt
@@ -12,14 +12,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
+        val success = findViewById<Button>(R.id.success)
+        val error = findViewById<Button>(R.id.error)
+        val warning = findViewById<Button>(R.id.warning)
+        val info = findViewById<Button>(R.id.info)
 
 
-        button.setOnClickListener {
+        success.setOnClickListener {
             MotionToast.createToast(this,
                 "Hurray success 😍",
                 "Upload Completed successfully!",
                 MotionToastStyle.SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+
+        }
+
+        error.setOnClickListener {
+            MotionToast.createToast(this,
+                "Failed ☹",
+                "Profile Update Failed!",
+                MotionToastStyle.ERROR,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+        }
+
+        warning.setOnClickListener {
+            MotionToast.createToast(this,
+                "Warning",
+                "Please fill all the details!",
+                MotionToastStyle.WARNING,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+        }
+
+        info.setOnClickListener {
+            MotionToast.createToast(this,
+                "This is information toast!",
+                "Upload Completed successfully!",
+                MotionToastStyle.INFO,
                 MotionToast.GRAVITY_BOTTOM,
                 MotionToast.LONG_DURATION,
                 ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
